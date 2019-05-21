dfcba46a705f75dc30c460e5177a5b2185f5f06a
jogoo8254 <kev_inc88@hotmail.com>
2019-05-21 10:05:34 -0700

    auctions controller

--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'auctions/create'
+  get 'auctions/show'
+  get 'auctions/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
